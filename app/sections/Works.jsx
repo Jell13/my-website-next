57a57425d8da3b544fbf104be30ec1eac856e6a1
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-11-27 13:57:18 -0800

    continuing on the work

--- app/sections/Works.jsx
+++ app/sections/Works.jsx
@@ -7,7 +7,7 @@ const Works = () => {
 
     const workRef = useRef()
 
-    const isInView = useInView(workRef, {margin: "-100px"})
+    const isInView = useInView(workRef, {once: true, margin: "-100px"})
 
     const wordVariants = {
       hidden: {y: 50, opacity: 0},
@@ -30,8 +30,8 @@ const Works = () => {
     ]
 
   return (
-    <section id='works' className='text-primary h-screen bg-secondary border-none -mt-1'>
-      <motion.div className='w-full md:px-10 flex flex-col'>
+    <section id='works' className='text-primary bg-secondary border-none -mt-1'>
+      <div className='w-full md:px-10 flex flex-col'>
         <motion.h1 
         className='text-primary lg:text-[6rem] md:text-[4rem]'
         ref={workRef}
@@ -49,7 +49,49 @@ const Works = () => {
               </motion.span>
             ))}
         </motion.h1>
-      </motion.div>
+
+        <div className='w-full text-primary mt-20 flex md:grid grid-cols-12'>
+          <div className='flex gap-20 col-span-7 md:col-start-6 sm:flex-row flex-col'>
+            <h3>Services</h3>
+            <p className='w-full font-medium leading-relaxed text-xl'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur eos perferendis iste neque doloremque voluptates magni officiis tempora necessitatibus autem
+            </p>
+          </div>
+        </div>
+        <div className='grid grid-cols-12 pt-12 gap-5'>
+            <div className='relative col-span-5'>
+              <div className='sticky top-12 text-[22vw] text-primary leading-[0.8] font-normal'>
+                <span className='relative'>0</span>
+                <div className='relative'>
+                  <div>
+                    <span className='inline-block'>
+                      1
+                      .
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+            <aside className='relative col-span-7'>
+              <div>
+                <a href="">
+                  <div>
+                    <img src="https://www.lifewire.com/thmb/Uqi3NYSBuoCojLl65_it-e5NmVg=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/keyboard-light-up-razer-gaming-2b6aa4b392184326925211f0cae4d6b9.jpg" alt="" />
+                  </div>
+                </a>
+              </div>
+              <div>
+                <a href="">
+                  <div>
+                    <img src="https://i.pinimg.com/736x/1f/5a/2a/1f5a2ae53ad9cc5c4051d34b79b12321.jpg" alt="" />
+                  </div>
+                </a>
+              </div>
+              <div></div>
+            </aside>
+        </div>
+      </div>
     </section>
   )
 }
