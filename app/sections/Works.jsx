fd96c3a9a4cf494326f5838e6458f88ebbe03a06
Jason Sugiharto <jasonliesugiharto@gmail.com>
2025-01-11 14:56:08 -0800

    added the moving cursor still needs fixing

--- app/sections/Works.jsx
+++ app/sections/Works.jsx
@@ -1,7 +1,7 @@
 "use client"
 
-import React, { useEffect, useRef } from 'react'
-import { motion, useInView } from 'framer-motion'
+import React, { useEffect, useRef, useState } from 'react'
+import { motion, useInView, useMotionValue } from 'framer-motion'
 
 const Works = () => {
 
@@ -28,6 +28,27 @@ const Works = () => {
       "WORKS",
       "/"
     ]
+
+    const [hovered, setHovered] = useState(false)
+    // const [cursorPosition, setCursorPosition] = useState({x: 0, y: 0})
+
+    // const handleMouseMove = (e) => {
+    //   setCursorPosition({x: e.clientX, y: e.clientY})
+    // }
+    const cursorSize = 50
+    const mousePosition = {
+      x: useMotionValue(0),
+      y: useMotionValue(0)
+    }
+    const manageMouseMove = (e) => {
+      mousePosition.x.set(e.clientX - cursorSize / 2),
+      mousePosition.y.set(e.clientY - cursorSize / 2)
+    }
+
+    useEffect(() => {
+      window.addEventListener("mousemove", manageMouseMove)
+      return () => window.removeEventListener("mousemove", manageMouseMove)
+    })
 
   return (
     <section id='works' className='text-primary bg-secondary border-none -mt-1'>
@@ -61,12 +82,17 @@ const Works = () => {
         </div>
         <div className='grid grid-cols-12 pt-12 gap-5 mt-10'>
             {/* First project*/}
-            <div className='relative col-span-12 mb-20'>
+            {/* <div className='col-span-3 col-start-3 mb-20'>
               <div>
                 <span className='text-[12vw] hidden lg:block text-thin_text'>01.</span>
               </div>
+            <div className='lg:col-span-7'>
               <div className='flex flex-col gap-8'>
-                <a href="" className='flex flex-col'>
+                <a 
+                onMouseEnter={(e) => setHovered(true)} 
+                onMouseLeave={(e) => setHovered(false)} 
+                // onMouseMove={handleMouseMove}
+                href="" className='flex flex-col'>
                   <div className='relative flex justify-center items-center'>
                     <img 
                     className='h-full w-full object-cover'
@@ -75,6 +101,14 @@ const Works = () => {
                     <div className='absolute z-10 text-white text-2xl'>
                       Vid of project here
                     </div>
+                    <motion.div
+                    className="absolute left-0 top-0 bg-gray-800 text-white p-2 rounded-xl"
+                    style={{x: mousePosition.x, y: mousePosition.y}}
+                    initial={false}
+                    animate={{scale: hovered ? 1 : 0}}
+                    >
+                      View
+                    </motion.div>
                   </div>
                 </a>
                 <div className='flex gap-8'>
@@ -85,6 +119,43 @@ const Works = () => {
                   <div className='flex items-end'>
                     <div className='border-[1px] border-third bg-third rounded-xl text-primary px-2'>2024</div>
                   </div>
+                </div>
+              </div>
+              </div>
+            </div> */}
+
+            <div className='col-span-3 col-start-3 lg:block hidden mb-20'>
+              <div>
+                <span className='text-[12vw] text-thin_text'>01.</span>
+              </div>
+            </div>
+            <div className='lg:col-span-7 relative col-span-12 mb-20'>
+              <a 
+              onMouseEnter={(e) => setHovered(true)}
+              onMouseLeave={(e) => setHovered(false)}
+              href="">
+                <div className='flex justify-center items-center'>
+                  <img src="https://www.lifewire.com/thmb/Uqi3NYSBuoCojLl65_it-e5NmVg=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/keyboard-light-up-razer-gaming-2b6aa4b392184326925211f0cae4d6b9.jpg" alt="" loading='lazy'/>
+                  <div className='absolute z-10 text-white text-2xl'>
+                    Vid of project here
+                  </div>
+                  <motion.div
+                    className="absolute left-10 top-0 bg-gray-800 text-white p-2 rounded-xl"
+                    style={{x: mousePosition.x, y: mousePosition.y}}
+                    initial={false}
+                    animate={{scale: hovered ? 1 : 0}}
+                    >
+                    View
+                  </motion.div>
+                </div>
+              </a>
+              <div className='flex gap-8'>
+                <div className='flex flex-col'>
+                  <h5 className='text-2xl text-thin_text'>Productivity | Collaboration</h5>
+                  <h4 className='font-medium text-4xl'>Workspace Collaborator | LiveDocs</h4>
+                </div>
+                <div className='flex items-end'>
+                  <div className='border-[1px] border-third bg-third rounded-xl text-primary px-2'>2024</div>
                 </div>
               </div>
             </div>
